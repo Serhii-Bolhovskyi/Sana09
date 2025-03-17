99159500eb962cf62fb1473520c800e83106f70e
Serhii-Bolhovskyi <azeror72@gmail.com>
2025-03-17 22:32:22 +0100

    Add Method that counts movies on page

--- Sana09/Program.cs
+++ Sana09/Program.cs
@@ -4,13 +4,16 @@ namespace Sana09;
 
 class Program
 {
+    public static class HttpHelper
+    {
+        public static readonly HttpClient HttpClientBoxOffice = new();
+        public static readonly string BoxOfficeUrl = "https://www.boxofficemojo.com/year/2024/?area=UA&grossesOption=totalGrosses";
+    }
+    
     // Ukrainian Box Office For 2024
     static public async Task<int> GetTotalGrosses()
     {
-        using HttpClient httpClient = new();
-        string url = "https://www.boxofficemojo.com/year/2024/?area=UA&grossesOption=totalGrosses";
-        
-        var response = await httpClient.GetStringAsync(url);
+        var response = await HttpHelper.HttpClientBoxOffice.GetStringAsync(HttpHelper.BoxOfficeUrl);
         int total = 0;
 
         string pattern = @"\$[\d,]+";
@@ -25,13 +28,38 @@ class Program
                 total += value;
             }
         }
-
+        Console.WriteLine($"Загальний BoxOffice: ${total}");
         return total;
+    }
+
+    // All Movies in Ukrainian Box Office For 2024
+    static public async Task<int> CountMoviesOnPage()
+    {
+        var response = await HttpHelper.HttpClientBoxOffice.GetStringAsync(HttpHelper.BoxOfficeUrl);
+        if (string.IsNullOrWhiteSpace(response))
+        {
+            Console.WriteLine("Помилка: Порожня відповідь від сервера.");
+            return 0;
+        }
+
+        string pattern = @"<td class=""a-text-left mojo-field-type-release mojo-cell-wide""[^>]*><a class=""a-link-normal""[^>]*>(.*?)</a></td>";
+        
+        int totalPages = Regex.Matches(response, pattern).Count;
+        Console.WriteLine($"Загальну к-ть фільмів: {totalPages}");
+        
+        return totalPages;
     }
     static async Task Main()
     {
-        Console.WriteLine("Загальну сума BoxOffice в Україні 2024");
-        var totalGrosses = await GetTotalGrosses();
-        Console.WriteLine($"Загальний BoxOffice: ${totalGrosses}");
+        var tasks = new List<Task>
+        {
+            GetTotalGrosses(),
+            CountMoviesOnPage()
+        };
+        
+        await Task.WhenAll(tasks);
+        Console.WriteLine("Всі запити завершено!");
+        
+        
     }
 }
